339931c09f26f4039d64b4b37dea22d3117e970c
eduardocheslackidurante <160148439+eduardocheslackidurante@users.noreply.github.com>
2024-10-18 00:13:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,23 +8,3 @@ function menuShow() {
         document.querySelector('.icon').src = "imagens/close_white_36dp.svg";
     }
 }
-function pesquisar() {
-    // Obtém o valor da pesquisa
-    var termoPesquisa = document.getElementById('pesquisa').value.toLowerCase();
-    
-    // Obtém todos os cartões de produto
-    var produtos = document.querySelectorAll('.card');
-
-    // Loop para verificar se o produto corresponde à pesquisa
-    produtos.forEach(function(produto) {
-        var nomeProduto = produto.querySelector('h3').textContent.toLowerCase();
-        var descricaoProduto = produto.querySelector('p').textContent.toLowerCase();
-
-        // Se o nome ou descrição contiver o termo da pesquisa, mostra o produto; senão, oculta
-        if (nomeProduto.includes(termoPesquisa) || descricaoProduto.includes(termoPesquisa)) {
-            produto.style.display = 'block'; // Mostra o produto
-        } else {
-            produto.style.display = 'none'; // Oculta o produto
-        }
-    });
-}
